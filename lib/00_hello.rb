0aa532daa4071af6d3dd8fcf5bf9f4e30ecc1b2a
maxis-i <ismaelrazafindramboly@gmail.com>
2019-10-08 11:20:45 +0300

    Modification du fichier 00_hello

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,10 @@
+#retourne la chaine de caractere hello
 def hello
-
+    return "Hello!"
 end
 
+#retourne la chaine de caractere en fonction du argument name
 def greet(name)
+    return "Hello, #{name}!"
 
 end
